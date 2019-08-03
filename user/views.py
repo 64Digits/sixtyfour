5f765b640ad2c52e5448fa34c50d8f37c5ccca19
Mitchell Burdette <mitchell.burdette@gmail.com>
2019-08-03 16:27:26 -0500

    Project templates and static files support

--- user/views.py
+++ user/views.py
@@ -3,14 +3,16 @@ from django.http import HttpResponse
 from django.utils import timezone
 from .models import Post, Comment
 
+from django.contrib.auth import get_user_model
+
 # testing a couple views
 
 def post_listing(request, username, page=1):
 	# TODO: use django pagination class
-	# TODO: create templates
+	user = get_user_model().objects.get(username=username)
 	posts = Post.objects.filter(user__username=username).order_by('-created')[:10]
-	return HttpResponse(str(posts),content_type="text/plain")
-	#return render(request, 'user/index.html', {'posts':posts})
+	#return HttpResponse(str(posts),content_type="text/plain")
+	return render(request, 'user/listing.html', {'user':user,'posts':posts})
 
 def post_detail(request, username, entry):
 	# TODO: create templates

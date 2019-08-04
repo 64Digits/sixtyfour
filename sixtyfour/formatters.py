27783f8b1fa60ff4fc9064601d7f69bb7e4ba935
Mitchell Burdette <mitchell.burdette@gmail.com>
2019-08-04 17:25:40 -0500

    Post preview on front page

--- sixtyfour/formatters.py
+++ sixtyfour/formatters.py
@@ -1,7 +1,9 @@
 from bbcode import Parser
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 
 def bbcode64(entry):
 	parser = Parser(newline='</p><p>')
-	
-	return '<p>'+parser.format(entry.entry)+'</p>'
+	entry = parser.format(entry.entry)
+	return format_html('<p>{}</p>',mark_safe(entry))
 
--- sixtyfour/tags.py
+++ sixtyfour/tags.py
@@ -7,13 +7,8 @@ register = template.Library()
 
 @register.inclusion_tag('include/pagination.html', takes_context=True)
 def pagination(context, page, view, *args, **kwargs):
-	#view = kwargs['view']
 	context['view'] = view
-	#kwargs.pop('view', None)
-	
-	#page = kwargs['page']
 	context['page'] = page
-	#kwargs.pop('page', None)
 	
 	if page.has_previous():
 		context['page_first'] = reverse(view, kwargs=kwargs)
@@ -32,4 +27,4 @@ def pagination(context, page, view, *args, **kwargs):
 def user(user):
 	url=reverse('user:listing', kwargs={'username':user.username})
 	return format_html('<a href="{}">{}</a>',mark_safe(url),user.username)
-	
+

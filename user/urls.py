2ac1e0773891142a5cf92c17beaabff2d907e241
Mitchell Burdette <mitchell.burdette@gmail.com>
2019-08-03 23:35:44 -0500

    Use url tag for pagination

--- user/urls.py
+++ user/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('<username>', views.post_listing),
-	path('<username>/page/<int:page>', views.post_listing),
-	path('<username>/post/<int:entry>', views.post_detail),
+	path('<username>', views.post_listing, name='user'),
+	path('<username>/page/<int:page>', views.post_listing, name='user-listing'),
+	path('<username>/post/<int:entry>', views.post_detail, name='user-post'),
 ]
--- user/views.py
+++ user/views.py
@@ -6,10 +6,7 @@ from .models import Post, Comment
 
 from django.contrib.auth import get_user_model
 
-# testing a couple views
-
 def post_listing(request, username, page=1):
-	# TODO: use django pagination class
 	user = get_user_model().objects.get(username=username)
 
 	# QuerySets are already optimized for pagination (E.G: They lazy load as needed,
@@ -20,10 +17,9 @@ def post_listing(request, username, page=1):
 	paginator = Paginator(post_set, 10)
 	posts = paginator.get_page(page)
 
-	return render(request, 'user/listing.html', {'user':user,'posts':posts, 'page':page})
+	return render(request, 'user/listing.html', {'user':user, 'posts':posts, 'page':page})
 
 def post_detail(request, username, entry):
 	post = Post.objects.get(user__username=username, id=entry)
-	print(post)
 	return render(request, 'user/post.html', {'post':post})
 
